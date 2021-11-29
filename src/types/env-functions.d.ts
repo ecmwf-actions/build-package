a27491b8530654d7253a07fa262a4f2922185791
Dusan Vuckovic <dusan.vuckovic@ecmwf.int>
2021-11-29 16:41:51 +0000

    Migrated code to TypeScript (WIP).
    [skip ci]

--- src/types/env-functions.d.ts
+++ src/types/env-functions.d.ts
@@ -1,4 +1,5 @@
-type EnvironmentVariables = Record<string, string>;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type EnvironmentVariables = Record<string, any>;
 export function setupEnv(os: string, compilerCc: string, compilerCxx: string, compilerFc: string): Promise<EnvironmentVariables>;
 export function extendPaths(env: EnvironmentVariables, installDir: string, packageName: string): void;
 export function extendDependencies(env: EnvironmentVariables, repository: string, sha: string): void;
